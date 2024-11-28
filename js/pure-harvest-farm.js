dbe68dced9d1e2c2e71cb15703684c99b7905c20
Susil Phago <sushil.phago123@gmail.com>
2024-11-28 15:26:01 +1100

    Validate footer form, display the specific message

--- js/pure-harvest-farm.js
+++ js/pure-harvest-farm.js
@@ -40,3 +40,37 @@ function closeView() {
     document.getElementById("fullimage").src = "";
     document.getElementById("fullvideo").src = "";
 }
+
+// validate subscribe to newsletter form of footer section
+document.getElementById('subscribe-button').addEventListener('click', function () {
+    const emailInput = document.getElementById('subscribe-email');
+    const checkboxInput = document.getElementById('subscribe-checkbox');
+    const successMessage = document.getElementById('success-message');
+    const failedMessage = document.getElementById('failed-message');
+    
+    // Validate email format
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailInput == "" || !emailPattern.test(emailInput.value) || (!checkboxInput.checked)) {
+        failedMessage.style.display = 'block';
+        failedMessage.style.backgroundColor = '#e23718'
+
+        // Hide message after 3 seconds
+        setTimeout(() => {
+            failedMessage.style.display = 'none';
+        }, 3000);
+    }else{
+        // Show success message
+        successMessage.style.display = 'block';
+        successMessage.style.backgroundColor = '#087933'
+    
+        // Hide success message after 3 seconds
+        setTimeout(() => {
+            successMessage.style.display = 'none';
+        }, 3000);
+    
+        // Reset the form
+        document.getElementById('footer-form').reset();
+
+    }
+
+});
